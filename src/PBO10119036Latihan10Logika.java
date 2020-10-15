8a80f0a8b624e53dcf21d9c78e1bc583a960b5bc
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 13:17:48 +0700

    Project 10

---
+++ src/PBO10119036Latihan10Logika.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKKAN DATA LOGIKA
+ */
+public class PBO10119036Latihan10Logika {
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
